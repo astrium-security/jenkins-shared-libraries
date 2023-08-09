468a44cd44a3c09859fc157d7090a7f92bdd6b4b
Sacha IFRAH <sacha_ifrah@carrefour.com>
2023-08-09 18:16:44 +0200

    modules

--- vars/defaultDeploymentParams.groovy
+++ vars/defaultDeploymentParams.groovy
@@ -5,7 +5,8 @@ def call(String name = 'human') {
     parameters([
         choice(name: 'INFRA_ENV', choices: ['NOPROD', 'PROD'], description: 'Which infra env to choose'),
         choice(name: 'TERRAFORM_ACTION', choices: ['PLAN', 'APPLY', 'DESTROY'], description: 'Which terraform action to perform'),
-        choice(name: 'CSPROVIDER', choices: ['AWS', 'GCP'], description: 'Which provider to choose')
+        choice(name: 'CSPROVIDER', choices: ['AWS', 'GCP'], description: 'Which provider to choose'),
+        string(name: 'PREFIX_PROJECT', defaultValue: '', description: 'The prefix of the project')
     ])
   ])
 }
